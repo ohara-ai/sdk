6f3d84ec58142431b85e53be17e5f681f80f828b
kanshi <46557+kanshi@users.noreply.github.com>
2025-11-21 15:43:43 +0100

    e2e-test: Package upgrades

--- e2e-test/next.config.js
+++ e2e-test/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    instrumentationHook: true,
-  },
   // Skip static optimization for pages using Wagmi
   skipTrailingSlashRedirect: true,
   webpack: (config, { isServer }) => {
